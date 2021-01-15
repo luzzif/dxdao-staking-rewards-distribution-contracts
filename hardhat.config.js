30fba05b9411c5584377bf68b9c8dd9f34b37373
Federico Luzzi <fedeluzzi00@gmail.com>
2021-01-16 00:21:53 +0100

    chore: add deploy and verify hardhat task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-truffle5");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/deploy-and-verify");
+
+const infuraId = process.env.INFURA_ID;
 
 module.exports = {
+    networks: {
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${infuraId}`,
+        },
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${infuraId}`,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
     solidity: {
         compilers: [
             {
@@ -28,5 +42,8 @@ module.exports = {
     gasReporter: {
         currency: "USD",
         enabled: process.env.GAS_REPORT_ENABLED === "true",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
     },
 };
